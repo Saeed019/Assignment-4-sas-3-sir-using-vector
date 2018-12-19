58b6fcbf1bf46a5fc4e0b136c4023580ee403706
abusa <abusa@DESKTOP-1IOICRI>
2018-12-19 19:21:40 +0600

    Header file of Bubble sort is added

---
+++ BubbleSort.h
@@ -0,0 +1,12 @@
+#ifndef BUBBLESORT_H_INCLUDED
+#define BUBBLESORT_H_INCLUDED
+#include<vector>
+using namespace std;
+class BubbleSort
+{
+public:
+    BubbleSort();
+    ~BubbleSort();
+    void BubbleSorter(vector<int> arr, int n);
+};
+#endif // BUBBLESORT_H_INCLUDED
